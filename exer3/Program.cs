6328e5661ae037e8df91128656d8d86cd8996ccd
lopesacm <mariaaclaraalopesss@gmail.com>
2025-03-21 08:15:41 -0300

    exer2

---
+++ exer3/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Escolha uma classe para iniciar a partida: ");
+Console.WriteLine("1-Guerreira");
+Console.WriteLine("2-Mago");
+Console.WriteLine("3-Arqueiro");
+Console.WriteLine("Digite a classe escolhida:");
+
+
